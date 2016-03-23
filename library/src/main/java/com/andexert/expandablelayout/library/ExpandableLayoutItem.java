dec8b930cca074002b9772b1fd48ffe058985043
Aleksandr Pokhilko <alex-pohilko@mail.ru>
2016-03-23 10:02:06 +0300

    Added expand/collapse listener

--- library/src/main/java/com/andexert/expandablelayout/library/ExpandableLayoutItem.java
+++ library/src/main/java/com/andexert/expandablelayout/library/ExpandableLayoutItem.java
@@ -1,18 +1,18 @@
 /***********************************************************************************
  * The MIT License (MIT)
-
+ * <p/>
  * Copyright (c) 2014 Robin Chutaux
-
+ * <p/>
  * Permission is hereby granted, free of charge, to any person obtaining a copy
  * of this software and associated documentation files (the "Software"), to deal
  * in the Software without restriction, including without limitation the rights
  * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
  * copies of the Software, and to permit persons to whom the Software is
  * furnished to do so, subject to the following conditions:
-
+ * <p/>
  * The above copyright notice and this permission notice shall be included in
  * all copies or substantial portions of the Software.
-
+ * <p/>
  * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
  * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
  * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
@@ -35,8 +35,7 @@ import android.view.animation.Transformation;
 import android.widget.FrameLayout;
 import android.widget.RelativeLayout;
 
-public class ExpandableLayoutItem extends RelativeLayout
-{
+public class ExpandableLayoutItem extends RelativeLayout {
     private Boolean isAnimationRunning = false;
     private Boolean isOpened = false;
     private Integer duration;
@@ -44,25 +43,29 @@ public class ExpandableLayoutItem extends RelativeLayout
     private FrameLayout headerLayout;
     private Boolean closeByUser = true;
 
-    public ExpandableLayoutItem(Context context)
-    {
+    private OnItemExpandListener mOnItemExpandListener;
+
+    public interface OnItemExpandListener {
+        void onExpand(ExpandableLayoutItem view);
+
+        void onCollapse(ExpandableLayoutItem view);
+    }
+
+    public ExpandableLayoutItem(Context context) {
         super(context);
     }
 
-    public ExpandableLayoutItem(Context context, AttributeSet attrs)
-    {
+    public ExpandableLayoutItem(Context context, AttributeSet attrs) {
         super(context, attrs);
         init(context, attrs);
     }
 
-    public ExpandableLayoutItem(Context context, AttributeSet attrs, int defStyle)
-    {
+    public ExpandableLayoutItem(Context context, AttributeSet attrs, int defStyle) {
         super(context, attrs, defStyle);
         init(context, attrs);
     }
 
-    private void init(final Context context, AttributeSet attrs)
-    {
+    private void init(final Context context, AttributeSet attrs) {
         final View rootView = View.inflate(context, R.layout.view_expandable, this);
         headerLayout = (FrameLayout) rootView.findViewById(R.id.view_expandable_headerlayout);
         final TypedArray typedArray = context.obtainStyledAttributes(attrs, R.styleable.ExpandableLayout);
@@ -86,13 +89,10 @@ public class ExpandableLayoutItem extends RelativeLayout
         contentLayout.addView(contentView);
         contentLayout.setVisibility(GONE);
 
-        headerLayout.setOnTouchListener(new OnTouchListener()
-        {
+        headerLayout.setOnTouchListener(new OnTouchListener() {
             @Override
-            public boolean onTouch(View v, MotionEvent event)
-            {
-                if (isOpened() && event.getAction() == MotionEvent.ACTION_UP)
-                {
+            public boolean onTouch(View v, MotionEvent event) {
+                if (isOpened() && event.getAction() == MotionEvent.ACTION_UP) {
                     hide();
                     closeByUser = true;
                 }
@@ -103,19 +103,20 @@ public class ExpandableLayoutItem extends RelativeLayout
 
     }
 
-    private void expand(final View v)
-    {
+    public void setOnItemExpandListener(OnItemExpandListener onItemExpandListener) {
+        mOnItemExpandListener = onItemExpandListener;
+    }
+
+    private void expand(final View v) {
         isOpened = true;
         v.measure(LayoutParams.MATCH_PARENT, LayoutParams.WRAP_CONTENT);
         final int targetHeight = v.getMeasuredHeight();
         v.getLayoutParams().height = 0;
         v.setVisibility(VISIBLE);
 
-        Animation animation = new Animation()
-        {
+        Animation animation = new Animation() {
             @Override
-            protected void applyTransformation(float interpolatedTime, Transformation t)
-            {
+            protected void applyTransformation(float interpolatedTime, Transformation t) {
                 v.getLayoutParams().height = (interpolatedTime == 1) ? LayoutParams.WRAP_CONTENT : (int) (targetHeight * interpolatedTime);
                 v.requestLayout();
             }
@@ -128,23 +129,21 @@ public class ExpandableLayoutItem extends RelativeLayout
         };
         animation.setDuration(duration);
         v.startAnimation(animation);
+        if (mOnItemExpandListener != null)
+            mOnItemExpandListener.onExpand(this);
     }
 
-    private void collapse(final View v)
-    {
+    private void collapse(final View v) {
         isOpened = false;
         final int initialHeight = v.getMeasuredHeight();
-        Animation animation = new Animation()
-        {
+        Animation animation = new Animation() {
             @Override
             protected void applyTransformation(float interpolatedTime, Transformation t) {
-                if(interpolatedTime == 1)
-                {
+                if (interpolatedTime == 1) {
                     v.setVisibility(View.GONE);
                     isOpened = false;
-                }
-                else{
-                    v.getLayoutParams().height = initialHeight - (int)(initialHeight * interpolatedTime);
+                } else {
+                    v.getLayoutParams().height = initialHeight - (int) (initialHeight * interpolatedTime);
                     v.requestLayout();
                 }
             }
@@ -157,70 +156,62 @@ public class ExpandableLayoutItem extends RelativeLayout
 
         animation.setDuration(duration);
         v.startAnimation(animation);
+        if (mOnItemExpandListener != null)
+            mOnItemExpandListener.onCollapse(this);
     }
 
-    public void hideNow()
-    {
+    public void hideNow() {
         contentLayout.getLayoutParams().height = 0;
         contentLayout.invalidate();
         contentLayout.setVisibility(View.GONE);
         isOpened = false;
+        if (mOnItemExpandListener != null)
+            mOnItemExpandListener.onCollapse(this);
     }
 
-    public void showNow()
-    {
-        if (!this.isOpened())
-        {
+    public void showNow() {
+        if (!this.isOpened()) {
             contentLayout.setVisibility(VISIBLE);
             this.isOpened = true;
             contentLayout.getLayoutParams().height = LayoutParams.WRAP_CONTENT;
             contentLayout.invalidate();
+            if (mOnItemExpandListener != null)
+                mOnItemExpandListener.onExpand(this);
         }
     }
 
-    public Boolean isOpened()
-    {
+    public Boolean isOpened() {
         return isOpened;
     }
 
-    public void show()
-    {
-        if (!isAnimationRunning)
-        {
+    public void show() {
+        if (!isAnimationRunning) {
             expand(contentLayout);
             isAnimationRunning = true;
-            new Handler().postDelayed(new Runnable()
-            {
+            new Handler().postDelayed(new Runnable() {
                 @Override
-                public void run()
-                {
+                public void run() {
                     isAnimationRunning = false;
                 }
             }, duration);
         }
     }
 
-    public FrameLayout getHeaderLayout()
-    {
+    public FrameLayout getHeaderLayout() {
         return headerLayout;
     }
 
-    public FrameLayout getContentLayout()
-    {
+    public FrameLayout getContentLayout() {
         return contentLayout;
     }
 
-    public void hide()
-    {
-        if (!isAnimationRunning)
-        {
+    public void hide() {
+        if (!isAnimationRunning) {
             collapse(contentLayout);
             isAnimationRunning = true;
-            new Handler().postDelayed(new Runnable()
-            {
+            new Handler().postDelayed(new Runnable() {
                 @Override
-                public void run()
-                {
+                public void run() {
                     isAnimationRunning = false;
                 }
             }, duration);
@@ -228,8 +219,7 @@ public class ExpandableLayoutItem extends RelativeLayout
         closeByUser = false;
     }
 
-    public Boolean getCloseByUser()
-    {
+    public Boolean getCloseByUser() {
         return closeByUser;
     }
 }
--- sample/src/main/java/com/andexert/expandablelayout/MainActivity.java
+++ sample/src/main/java/com/andexert/expandablelayout/MainActivity.java
@@ -1,27 +1,38 @@
 package com.andexert.expandablelayout;
 
 import android.app.Activity;
+import android.content.Context;
+import android.graphics.Color;
 import android.os.Bundle;
+import android.view.LayoutInflater;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.TextView;
 
+import com.andexert.expandablelayout.library.ExpandableLayoutItem;
 import com.andexert.expandablelayout.library.ExpandableLayoutListView;
 
 
 public class MainActivity extends Activity {
 
-    private final String[] array = {"Hello", "World", "Android", "is", "Awesome", "World", "Android", "is", "Awesome", "World", "Android", "is", "Awesome", "World", "Android", "is", "Awesome"};
+    private final String[] array = {"Hello", "World", "Android", "is", "Awesome", "World", "Android", "is", "Awesome", "World", "Android", "is", "Awesome", "World", "Android", "is", "Awesome", "Hello", "World", "Android", "is", "Awesome", "World", "Android", "is", "Awesome", "World", "Android", "is", "Awesome", "World", "Android", "is", "Awesome", "Hello", "World", "Android", "is", "Awesome", "World", "Android", "is", "Awesome", "World", "Android", "is", "Awesome", "World", "Android", "is", "Awesome"};
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        final ArrayAdapter<String> arrayAdapter = new ArrayAdapter<String>(this, R.layout.view_row, R.id.header_text, array);
+        StringAdapter adapter = new StringAdapter(this, array);
         final ExpandableLayoutListView expandableLayoutListView = (ExpandableLayoutListView) findViewById(R.id.listview);
 
-        expandableLayoutListView.setAdapter(arrayAdapter);
+        expandableLayoutListView.setAdapter(adapter);
+
+        expandableLayoutListView.addHeaderView(new Button(this), null, false);
+        expandableLayoutListView.addFooterView(new Button(this), null, false);
     }
 
 
@@ -42,5 +53,35 @@ public class MainActivity extends Activity {
             return true;
         }
         return super.onOptionsItemSelected(item);
+    }
+
+    private class StringAdapter extends ArrayAdapter<String> implements ExpandableLayoutItem.OnItemExpandListener {
+        private LayoutInflater mInflater;
+
+        public StringAdapter(Context context, String[] items) {
+            super(context, R.layout.view_row, items);
+            mInflater = LayoutInflater.from(context);
+        }
+
+        @Override
+        public View getView(int position, View convertView, ViewGroup parent) {
+            if (convertView == null)
+                convertView = mInflater.inflate(R.layout.view_row, parent, false);
+            ExpandableLayoutItem expItem = (ExpandableLayoutItem) convertView.findViewById(R.id.row);
+            expItem.setOnItemExpandListener(this);
+            TextView tv = (TextView) convertView.findViewById(R.id.header_text);
+            tv.setText(getItem(position));
+            return convertView;
+        }
+
+        @Override
+        public void onExpand(ExpandableLayoutItem view) {
+            view.setBackgroundColor(Color.GREEN);
+        }
+
+        @Override
+        public void onCollapse(ExpandableLayoutItem view) {
+            view.setBackgroundColor(Color.parseColor("#e74c3c"));
+        }
     }
 }
